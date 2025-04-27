93eed8e9c49f2c4509456bd746086a1732ba5cd5
gttome <130812356+gttome@users.noreply.github.com>
2025-04-27 17:04:03 -0500

    Add files via upload

---
+++ prompt-structure/Start Server.bat
@@ -0,0 +1,12 @@
+@echo off
+pushd "%~dp0"
+
+:: Launch Anaconda PowerShell, activate base env, start server
+start "CondaServer" ^
+ "%windir%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoExit -ExecutionPolicy ByPass -Command ^
+ "& 'C:\Users\georg\anaconda3\shell\condabin\conda-hook.ps1'; conda activate 'C:\Users\georg\anaconda3'; python -m http.server 8000; Write-Host 'Press Ctrl+C to quit…'; pause"
+
+:: Open your browser
+start "" http://localhost:8000/prompt-structure.html
+
+popd
