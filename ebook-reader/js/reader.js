677bbf19cdec8bfed5a32c233dad1151e1749adf
George <130812356+gttome@users.noreply.github.com>
2025-05-30 17:29:53 -0500

    Delete ebook-reader/js/reader.js

--- ebook-reader/js/reader.js
+++
@@ -1,8 +0,0 @@
-import ProgressIndicator from './ProgressIndicator';
-import { readerState } from './state';
-
-// After DOM is ready and #reader-header exists:
-const progressContainer = document.createElement('div');
-progressContainer.id = 'progress-indicator';
-document.querySelector('#reader-header').appendChild(progressContainer);
-new ProgressIndicator({ container: progressContainer, readerState });
